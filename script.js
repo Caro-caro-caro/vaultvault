5a6bc3b69f9e78087e6c5cb52ed93e7b5f9e9846
macbook <cmmarcan@gmail.com>
2025-02-18 11:47:55 -0800

    committing updates file

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 //write the psedocode for what I am doing in psedocode here//
-/*create strong message for user of the interface that tells them the specific 
-message of the assignment "You have received this message because you have been chosen to open an important vault.
-Her is the secret combination" */
-//assign 3 variables with arithmatic operations, each equalling one of the numerical values of the combination, which is : 10-40-39//
-//add comments to my work that the reader of the code can see//
-//Create a dialog box or an alert displaying the vault combination ad message//
-//Don't forget to submit assignment to Github??
+
+/*create string message for the user of the interface that tells them the specific 
+message of the assignment, which is; "You have received this message because you have been chosen
+to open an important vault. Here is the secret combination" */
+
+/*Assign 3 variables with arithmetic operations (+, -, *, /), each equalling one of
+the numerical values of the combination. The combination is : 10-40-39 */
+
+//add comments to my work so that the reader of the code can see and understand each step of the process//
+
+//Create a dialog box or an alert which displays the vault combination and message//
+
+//Don't forget to submit assignment to Github//
 
 let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
-//this is me writing the tring message//
+//this is me writing the string message//
 
 let num1 = 10+0;
 let num2 = 80/2;
 let num3 = 13*3;
-//these are the numerical values
+/*these are the variables (num1, num2, num3) which are numerical in type
+with arithmetical operations.*/
 
 alert(`${message} ${num1}-${num2}-${num3}`)
-//this creates the alert box that displays the message with the combination//
+/*this creates the alert box that displays what is in the parentheses, 
+which in this case is the message with the combination to the vault 
+expressed by the combination of the numerical variables defined
+in the previous section*/
 
 
 
