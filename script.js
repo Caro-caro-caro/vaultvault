6eef2464169e8f22376960bed3e92f23604bb5be
macbook <cmmarcan@gmail.com>
2025-02-18 11:24:06 -0800

    committing js vault combo practice files

---
+++ script.js
@@ -0,0 +1,23 @@
+//write the psedocode for what I am doing in psedocode here//
+/*create strong message for user of the interface that tells them the specific 
+message of the assignment "You have received this message because you have been chosen to open an important vault.
+Her is the secret combination" */
+//assign 3 variables with arithmatic operations, each equalling one of the numerical values of the combination, which is : 10-40-39//
+//add comments to my work that the reader of the code can see//
+//Create a dialog box or an alert displaying the vault combination ad message//
+//Don't forget to submit assignment to Github??
+
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//this is me writing the tring message//
+
+let num1 = 10+0;
+let num2 = 80/2;
+let num3 = 13*3;
+//these are the numerical values
+
+alert(`${message} ${num1}-${num2}-${num3}`)
+//this creates the alert box that displays the message with the combination//
+
+
+
+
